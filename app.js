75f4077bc9b26addb62114ef9c4bf3684cb61349
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-30 08:46:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const tabElActive=document.getElementsByClassName("tab-el-active")
 const tabLink=document.getElementsByClassName("tab-link")
 for (const el of tabEl) {el.addEventListener("click", e=>{
   console.log("Kliknięto!");
+  for (const el of tabElActive) {el.classList.remove("tab-el-active")};
+  el.classList.add("tab-el-active");
 })
 }
 
