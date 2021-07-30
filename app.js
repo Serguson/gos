4dc5e87dbd4fc7d1178eff14715e184a02bedd65
Serguson <84004791+Serguson@users.noreply.github.com>
2021-07-30 09:06:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const tabEl = document.getElementsByClassName("tab-el");
-const tabElActive=document.getElementsByClassName("tab-el-active")
-const tabLink=document.getElementsByClassName("tab-link")
+const tabElActive=document.getElementsByClassName("tab-el-active");
+const tabContent=document.getElementByClassName("tab-content");
+const tabContentActive = document.getElementsByClassName("tab-content-active");
+const intimacy = document.getElementById("intimacy");
+const charm = document.getElementById("charm");
+const talent=document.getElementById("talent");
 for (const el of tabEl) {el.addEventListener("click", e=>{
   console.log("Kliknięto!");
   for (const el of tabElActive) {el.classList.remove("tab-el-active")};
   el.classList.add("tab-el-active");
+  for (const el of tabContentActive) {el.classList.remove("tab-content-active")};
+  if (el.firstElementChild.getAttribute("href")==="#intimacy"){intimacy.classList.add("tab-content-active")}
+  else if (el.firstElementChild.getAttribute("href")==="#charm"){charm.classList.add("tab-content-active")}
+  else if (el.firstElementChild.getAttribute("href")==="#talent"){talent.classList.add("tab-content-active")}
 })
 }
 
